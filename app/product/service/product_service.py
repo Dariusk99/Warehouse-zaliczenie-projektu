541edae30048edd9e18165d39e5c2ed00f0ac7d0
Dariusk99 <dariuszkosnicki@gmail.com>
2025-11-17 22:03:07 +0100

    Feature: add rest api POST for register product to db

--- app/product/service/product_service.py
+++ app/product/service/product_service.py
@@ -1,4 +1,5 @@
 from app.product.repository.product_repository import ProductRepository
+from app.product.model.entity.product import Product
 
 class ProductService:
     def __init__(self, product_repository: ProductRepository):
@@ -6,3 +7,12 @@ class ProductService:
 
     def find_all(self):
         return self.product_repository.find_all()
+    
+    def save(self, data):
+        product = Product(**data)
+        saved_product = self.product_repository.save(product)
+
+        if not saved_product:
+            return None
+        
+        return saved_product
